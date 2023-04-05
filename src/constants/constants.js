a70fc5dd1fc148fa021160f86c7f963dfad7e6d5
Maarten <maartenhandels@gmail.com>
2023-04-05 23:52:07 +0100

    delete duplicate files

--- src/constants/constants.js
+++
@@ -1,5 +0,0 @@
-// Image Paths
-export const SingleRoomImagePath = "images/rooms/round/single.png";
-export const TwoBedsRoomImagePath = "images/rooms/round/two-beds.png";
-export const DoubleRoomImagePath = "images/rooms/round/double.png";
-export const TripleRoomImagePath = "images/rooms/round/triple.png";
